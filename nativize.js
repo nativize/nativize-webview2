a325d8bf4897da55d895ed791e071fb82ba47780
Garden Gim <50333551+gardengim@users.noreply.github.com>
2025-02-19 11:35:11 +0900

    Initial Commit

---
+++ nativize.js
@@ -0,0 +1,53 @@
+//PROTOTYPE
+
+export const check = async () => {
+  console.log("Checking webview 2...");
+  console.log("we check dependencies, development environments");
+  console.log("we need NuGet, MSBuild, Windows SDK, etc, commands");
+};
+
+export const prepare = async () => {
+  console.log("Preparing webview 2...");
+  console.log("Installing NuGet packages...");
+
+  try {
+    await new Deno.Command("nuget", {
+      args: [
+        "restore",
+        `${import.meta.dirname}/nativize-webview2.sln`,
+      ],
+    }).spawn().status;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const build = async () => {
+  //TODO: get rid of this
+  console.log("Building webview 2...");
+
+  try {
+    await new Deno.Command("msbuild", {
+      args: [
+        `${import.meta.dirname}/nativize-webview2.sln`,
+      ],
+    }).spawn().status;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const run = async () => {
+  //TODO: get rid of this
+  console.log("Running webview 2...");
+
+  try {
+    //TODO: we need to implement some way to access several architecture directories
+    await new Deno.Command(
+      `${import.meta.dirname}/x64/Debug/nativize-webview2.exe`,
+      {},
+    ).spawn().status;
+  } catch (error) {
+    console.error(error);
+  }
+};
