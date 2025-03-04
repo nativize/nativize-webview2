e8c7e1c250acbfa37d816d682347586db0c7975b
Garden Gim <50333551+gardengim@users.noreply.github.com>
2025-03-04 12:18:04 +0900

    Remove try-catch

--- nativize.js
+++ nativize.js
@@ -8,59 +8,43 @@ export const prepare = async () => {
   console.log("Preparing webview 2...");
   console.log("Installing NuGet packages...");
 
-  try {
-    await new Deno.Command("nuget", {
-      args: [
-        "restore",
-        `${import.meta.dirname}/nativize-webview2.sln`,
-      ],
-    }).spawn().status;
-  } catch (error) {
-    console.error(error);
-  }
+  await new Deno.Command("nuget", {
+    args: [
+      "restore",
+      "nativize-webview2.sln",
+    ],
+    cwd: import.meta.dirname,
+  }).spawn().status;
 };
 
 export const build = async () => {
   //TODO: get rid of this
   console.log("Building webview 2...");
 
-  try {
-    await new Deno.Command("msbuild", {
-      cwd: import.meta.dirname
-    }).spawn().status;
-  } catch (error) {
-    console.error(error);
-  }
+  await new Deno.Command("msbuild", {
+    cwd: import.meta.dirname,
+  }).spawn().status;
 };
 
 export const run = async () => {
   //TODO: get rid of this
   console.log("Running webview 2...");
 
-  try {
-    //TODO: we need to implement some way to access several architecture directories
-    await new Deno.Command(
-      `${import.meta.dirname}/x64/Debug/nativize-webview2.exe`,
-      {},
-    ).spawn().status;
-  } catch (error) {
-    console.error(error);
-  }
+  //TODO: we need to implement some way to access several architecture directories
+  await new Deno.Command(
+    `${import.meta.dirname}/x64/Debug/nativize-webview2.exe`,
+    {},
+  ).spawn().status;
 };
-
 
 export const clean = async () => {
   console.log("Cleaning webview 2...");
 
-  try {
-    await new Deno.Command("msbuild", {
-      args: [
-        `nativize-webview2.sln`,
-        "/t:Clean",
-      ],
-      cwd: import.meta.dirname
-    }).spawn().status;
-  } catch (error) {
-    console.error(error);
-  }
-}
+  await new Deno.Command("msbuild", {
+    args: [
+      `nativize-webview2.sln`,
+      "/t:Clean",
+    ],
+    cwd: import.meta.dirname,
+  }).spawn().status;
+};
